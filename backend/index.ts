72fd316f7b3e490a1e95becde3df8260cc8a35ff
Natalia Camarinha <natcamarinha@gmail.com>
2022-12-08 20:14:57 -0300

    fix port

--- backend/index.ts
+++ backend/index.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import residentRouter from './src/routes/ResidentRouter';
 import error from './src/middlewares/error';
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = 7468;
+const PORT = process.env.MYSQLPORT || 3000;
 
 app.use('/', residentRouter);
 app.use(error);
